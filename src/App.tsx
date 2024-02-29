7ad9770fd03c4fd616de72d34d66a7b3eef5eacb
bobbyhadz <opmftw@gmail.com>
2024-02-29 15:45:58 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+      <input
+        type="text"
+        id="message"
+        defaultValue="Initial value"
+      />
+
+      <button>Click</button>
+    </div>
+  );
+}
